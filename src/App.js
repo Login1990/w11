2e699649b1cbf64aaada6b5932ebb79b93aee1f0
Login1990 <79404334+Login1990@users.noreply.github.com>
2024-02-01 14:28:45 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import MyContainer from './components/MyContainer';
+import About from './components/About';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+          <Routes>
+            <Route path="/" element={<><Header /> <MyContainer /></>} />
+            <Route path="/about" element={<><Header /> <About/></>} />
+          </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/MyContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MyContainer() {
+  return (
+    <div>MyContainer</div>
+  )
+}
+
+export default MyContainer
