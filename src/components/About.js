03866f7048fd74847046843015062c05e4a67612
Login1990 <79404334+Login1990@users.noreply.github.com>
2024-02-01 16:40:54 +0200

    Task 2

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import { useEffect, useState} from 'react'
 function About() {
-  return (
-    <div>About</div>
-  )
+    const [json, setJson] = useState([])
+
+    useEffect(() => {
+        let mounted = true
+        async function fetchItems(){
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+            const json = await response.json()
+            if(mounted){
+                setJson(json)
+            }
+        }
+        fetchItems()
+        return () => {
+            mounted = false
+        }
+    },[])
+    return (
+        <ul>
+            {
+                json.map((data) => (<li key={data.id} >{data.title}</li>))
+            }
+        </ul>
+    )
 }
 
 export default About
